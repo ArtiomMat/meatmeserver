a5fb4fa035f2e669561686dd5f5cb50ec174cdb3
doomguy <artiomat13100@gmail.com>
2023-01-28 01:12:53 +0200

    testing noise

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
 int main(int args_n, char **args) {
 	mle_map_t map;
 	mle_load_map(&map, "0.jpg");
-	mle_noise_map(&map, 50);
+	mle_noise_map_limited(&map, 100, 0, 1);
 	mle_save_map_jpeg(&map, "1.jpg", 100);
 	float p[3] = {1,2,3};
 	mle_get_map_pixel(&map, p, 316, 7);
